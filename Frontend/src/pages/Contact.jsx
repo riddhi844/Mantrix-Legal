36889583d7b0701f8cef91d08aecc63f5e4435da
riddhi jangid <riddhishiwad@gmail.com>
2025-12-30 22:43:54 +0530

    update backend api url

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -20,7 +20,8 @@ export default function Contact() {
         setLoading(true);
 
         try {
-            const res = await fetch("http://localhost:5000/api/contact", {
+            const res = await fetch("https://mantrix-backend.onrender.com/api/contact", {
+
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
